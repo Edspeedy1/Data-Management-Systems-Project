0988633e6260b5d7c5860941b581678bb5498880
Eric De Leenheer <105471603+Edspeedy1@users.noreply.github.com>
2024-11-06 20:23:57 -0500

    db editor changes

--- dbeditor.py
+++ dbeditor.py
@@ -3,9 +3,14 @@ import sqlite3
 conn = sqlite3.connect('database.db')
 c = conn.cursor()
 
+def create_tables():
+    c.execute('''CREATE TABLE IF NOT EXISTS loginInfo
+        (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, password TEXT NOT NULL)''')
 
-c.execute('''CREATE TABLE IF NOT EXISTS loginInfo
-    (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, password TEXT NOT NULL)''')
+def clear_tables():
+    # empty the tables but don't delete them
+    c.execute("DELETE FROM loginInfo")
+
 
 conn.commit()
 conn.close()
